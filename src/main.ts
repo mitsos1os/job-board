8b30c87f33d3fd6f95b9d733f41ba722bed1145d
Dimitris Halatsis <dimitris_ha@hotmail.com>
2021-04-26 12:52:01 +0300

    add global path prefix

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger,
   });
+  app.setGlobalPrefix('/api/v1');
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port');
   await app.listen(<number>port);
