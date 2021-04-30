4fe892ed0fbb1f1cef9a505524af0207bfdb4620
Dimitris Halatsis <dimitris_ha@hotmail.com>
2021-04-30 16:43:59 +0300

    shorten controller decoration

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,14 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import {
-  Crud,
-  CrudController,
-  CrudRequest,
-  Override,
-  ParsedRequest,
-  ParsedBody,
-  CrudAuth,
-} from '@nestjsx/crud';
+import { Crud, CrudController, CrudAuth } from '@nestjsx/crud';
 import { Company } from './company.entity';
 import { CompanyService } from './company.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
@@ -27,8 +19,15 @@ const persistUserId = (req: Request) => ({
 @Crud({
   model: { type: Company },
   query: { softDelete: true },
+  dto: { create: CreateCompanyDto, update: UpdateCompanyDto },
   routes: {
     exclude: ['replaceOneBase', 'createManyBase'],
+    getOneBase: { decorators: [Public()] },
+    getManyBase: { decorators: [Public()] },
+    createOneBase: { decorators: [ApiBearerAuth()] },
+    updateOneBase: { decorators: [ApiBearerAuth()] },
+    deleteOneBase: { decorators: [ApiBearerAuth()] },
+    recoverOneBase: { decorators: [ApiBearerAuth()] },
   },
   serialize: {
     get: CompanyResponseDto,
@@ -42,50 +41,4 @@ const persistUserId = (req: Request) => ({
 @Controller('companies')
 export class CompanyController implements CrudController<Company> {
   constructor(public service: CompanyService) {}
-
-  get base(): CrudController<Company> {
-    return this;
-  }
-
-  @Public()
-  @Override()
-  getOne(@ParsedRequest() req: CrudRequest) {
-    return this.base.getOneBase?.(req);
-  }
-
-  @Public()
-  @Override()
-  getMany(@ParsedRequest() req: CrudRequest) {
-    return this.base.getManyBase?.(req);
-  }
-
-  @ApiBearerAuth()
-  @Override()
-  createOne(
-    @ParsedRequest() req: CrudRequest,
-    @ParsedBody() dto: CreateCompanyDto,
-  ) {
-    return this.base.createOneBase?.(req, dto as Company);
-  }
-
-  @ApiBearerAuth()
-  @Override()
-  updateOne(
-    @ParsedRequest() req: CrudRequest,
-    @ParsedBody() dto: UpdateCompanyDto,
-  ) {
-    return this.base.updateOneBase?.(req, dto as Company);
-  }
-
-  @ApiBearerAuth()
-  @Override()
-  async deleteOne(@ParsedRequest() req: CrudRequest) {
-    return this.base.deleteOneBase?.(req);
-  }
-
-  @ApiBearerAuth()
-  @Override()
-  async recoverOne(@ParsedRequest() req: CrudRequest) {
-    return this.base.recoverOneBase?.(req);
-  }
 }
